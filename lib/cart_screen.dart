4594d8d680dca60adf0252b4de31710662296c48
Tuane <leao.gustavo075@gmail.com>
2025-02-11 11:44:48 -0300

    make scratch of cart_screen

--- lib/cart_screen.dart
+++ lib/cart_screen.dart
@@ -12,11 +12,61 @@ class _CartScreen extends State<CartScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Cart'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: TextField(
+          decoration: InputDecoration(
+            hintText: 'Pesquise por um produto aqui',
+            hintStyle: TextStyle(color: Colors.grey),
+            border: InputBorder.none,
+            suffixIcon: Icon(Icons.search, color: Colors.grey),
+          ),
+        ),
       ),
-      body: const Center(
-        child: Text('Cart Screen'),
-      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    child: ListTile(
+                      leading: Icon(Icons.image, size: 50),
+                      title: Text('Produto'),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(height: 5),
+                          Row(
+                            children: [
+                              Text('Quantidade: '),
+                              DropdownButton<int>(
+                                value: 1,
+                                items: List.generate(10, (index) {
+                                  return DropdownMenuItem(
+                                    value: index + 1,
+                                    child: Text('${index + 1}'),
+                                  );
+                                }),
+                                onChanged: (value) {},
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      trailing: Text(
+                        'R\$ 99,99',
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                );
+              },
+              childCount: 5,
+            ),
+          ),
+        ],
+      )
     );
   }
 }
